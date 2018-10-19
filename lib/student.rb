803fb07e3100a3afdcffdff4c6d827017f9f7adb
Robin Tram <robin.tram@gmail.com>
2018-10-19 18:06:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,9 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name:, grade:, id = nil)
+  def initialize(name:, grade:)
       @name = name 
       @grade = grade
-      @id = id
   end 
   
   def save
